012d05af66df341b9064816c0736b746a6842f4b
Diego <juandiguzfig@gmail.com>
2025-10-07 22:42:32 -0300

    TP5 - Lenguajes IV terminado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
-import ImageUploader from "./components/ImageUploader";
-import Contacto from "./components/Contacto";
+import Navbar from "./components/Navbar";
+import Inicio from "./pages/Inicio";
 import Servicios from "./components/Servicios";
+import Contacto from "./pages/Contacto";
+import API from "./pages/API";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<ImageUploader />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/servicios" element={<Servicios />} />
-      </Routes>
+      <div>
+        {/* Barra de navegación */}
+        <Navbar />
+
+        {/* Contenido principal */}
+        <div style={{ padding: "20px", textAlign: "center" }}>
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/servicios" element={<Servicios />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/api" element={<API />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
